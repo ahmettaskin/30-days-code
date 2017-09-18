383aeacc1498d294bb73fabdbcd06a452d45b110
ahmettaskin <ahmet.taskin@gkfx.com>
2017-09-18 13:10:03 +0300

    Day-4 is added.

---
+++ 30-days-code/Day-4/main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+class Person {
+public:
+	int age;
+	Person(int initAge);
+	void yearPasses();
+	void amIOld();
+};
+
+Person::Person(int initAge) {
+	if (initAge > 0) 
+		age = initAge;
+	else { 
+		cout << "Age is not valid, setting age to 0." << endl;
+		age = 0;
+	}
+	return;
+}
+void Person::yearPasses() {
+	age++;
+	return;
+}
+
+void Person::amIOld() {
+	if (age < 13) cout << "You are young." << endl;
+	else if (age < 18) cout << "You are a teenager." << endl;
+	else cout << "You are old." << endl;
+
+	return;
+}
+
+int main() {
+	int t;
+	int age;
+	cin >> t;
+	for (int i = 0; i < t; i++) {
+		cin >> age;
+		Person p(age);
+		p.amIOld();
+		for (int j = 0; j < 3; j++) {
+			p.yearPasses();
+		}
+		p.amIOld();
+
+		cout << '\n';
+	}
+
+	return 0;
+}
+
